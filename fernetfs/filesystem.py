c900546a6c753ba75b73f81fc88c05a241722b46
lmoulin <lmoulin@spartan-conseil.fr>
2022-09-11 10:23:16 +0200

    add filesystem

---
+++ fernetfs/filesystem.py
@@ -0,0 +1,97 @@
+from ast import Str
+import logging
+import os.path
+import os
+import glob
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+from fernetfs.file import File
+from fernetfs.directory import Directory
+from fernetfs.mastersalt import MasterSalt
+
+
+class FileSystem():
+    KEY_LENGTH = 256
+    SALT_LENGTH = 256
+    def __init__(self, secret:bytes, root_path:str, iterations:int = 480000, salt_size=16, sub_iterations:int = 48000):
+        self._secret = secret
+        self._root_path = root_path
+        self._iterations = iterations
+        self._salt_size = salt_size
+        self._sub_iterations = sub_iterations
+
+        self._key = None
+        self._salt = MasterSalt(secret, root_path, iterations, FileSystem.SALT_LENGTH)
+
+    def iscreated(self)->bool:
+        return self._salt.exists()
+
+    def create(self)->None:
+        path = os.path.join(self._root_path, "*")
+        ls_destination = list(glob.glob(path))
+
+        if len(ls_destination) > 0:
+            raise Exception("Mounting point is not empty")
+
+        self._salt.create()
+        self.mount()
+
+    def mount(self)->None:
+        if not self.iscreated():
+            raise Exception(f"File system at {self._root_path} doesn't exist")
+
+        salt = self._salt.get()
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=FileSystem.KEY_LENGTH,
+            salt=salt,
+            iterations=self._iterations,
+        )
+        self._key = kdf.derive(self._secret)
+
+    def _split_path(self, path:str)->list:
+        if path.startswith("/"):
+            path = path[1:]
+
+        return list(path.split("/"))
+
+    def _get_directory(self, relative_path:Str)->Directory:
+        path = self._root_path
+        directory = Directory(self._key, self._root_path, self._sub_iterations, self._salt_size)
+
+        sub_dirs = self._split_path(relative_path)
+        for sub_dir in sub_dirs[:-1]:
+            hash_name = directory.gethash(sub_dir)
+            path = os.path.join(path, hash_name)
+            directory = Directory(self._key, path, self._sub_iterations, self._salt_size)
+
+        return directory, sub_dirs[-1]
+
+    def mkdir(self, path:str)->None:
+        directory, last_dir = self._get_directory(path)
+        directory.mkdir(last_dir)
+
+    def open(self, path:str, mode:str)->File:
+        directory, filename = self._get_directory(path)
+        file = directory.get_file()
+        return file.open(filename, mode)
+
+    def remove_file(self, path:str):
+        directory, filename = self._get_directory(path)
+        file = directory.get_file()
+        file.rm(filename)
+
+    def remove_directory(self, path:str, recursive:bool=False):
+        directory, directory_name = self._get_directory(path)
+        directory.rm(directory_name, recursive)
+
+    def is_file_exist(self, path:str)->bool:
+        directory, filename = self._get_directory(path)
+        file = directory.get_file()
+        return file.exists(filename)
+
+    def is_file_exist(self, path:str)->bool:
+        directory, directoryname = self._get_directory(path)
+        return directory.exists(directoryname)
---
+++ test/test_filesystem.py
@@ -0,0 +1,128 @@
+from glob import glob
+import unittest
+import os
+import shutil
+import logging
+import os.path
+
+from fernetfs.filesystem import FileSystem
+
+WORKING_DIR = "/tmp/test_directory"
+SECRET = b"secret"
+ITERATIONS = 100
+SALT = 16
+
+logging.basicConfig(level=logging.DEBUG)
+
+class TestFileSystem(unittest.TestCase):
+    def setUp(self) -> None:
+        os.mkdir(WORKING_DIR)
+
+    def tearDown(self) -> None:
+        shutil.rmtree(WORKING_DIR)
+
+    def test_create(self)->dict:
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS)
+
+        fs.create()
+
+    def test_iscreated(self)->dict:
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS)
+
+        fs.create()
+        results = fs.iscreated()
+
+        self.assertTrue(results)
+
+    def test_mkdir(self)->dict:
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS, SALT, ITERATIONS)
+
+        fs.create()
+        fs.mkdir("foobar")
+
+    def test_mkdir_1_child(self)->dict:
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS, SALT, ITERATIONS)
+
+        fs.create()
+        fs.mkdir("foobar")
+        fs.mkdir("/foobar/hhh")
+
+    def test_mkdir_2_child(self)->dict:
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS, SALT, ITERATIONS)
+
+        fs.create()
+        fs.mkdir("foobar")
+        fs.mkdir("/foobar/hhh")
+        fs.mkdir("foobar/hhh/aaaa")
+
+    def test_open(self):
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS, SALT, ITERATIONS)
+        fs.create()
+
+        with fs.open("test.txt", "w") as f:
+            f.write("demo")
+
+        with fs.open("test.txt", "r") as f:
+            result = f.read()
+
+        expected = "demo"
+        self.assertEqual(result, expected)
+
+    def test_open_in_dir(self):
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS, SALT, ITERATIONS)
+        fs.create()
+
+        fs.mkdir("/foobar")
+
+        with fs.open("/foobar/test.txt", "w") as f:
+            f.write("demo")
+
+        with fs.open("/foobar/test.txt", "r") as f:
+            result = f.read()
+
+        expected = "demo"
+        self.assertEqual(result, expected)
+
+    def test_remove_file(self):
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS, SALT, ITERATIONS)
+        fs.create()
+
+        with fs.open("test.txt", "w") as f:
+            f.write("demo")
+
+        fs.remove_file("test.txt")
+
+    def test_remove_directory(self)->dict:
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS, SALT, ITERATIONS)
+
+        fs.create()
+        fs.mkdir("foobar")
+
+        fs.remove_directory("foobar")
+
+    def test_is_file_exist(self):
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS, SALT, ITERATIONS)
+        fs.create()
+
+        with fs.open("test.txt", "w") as f:
+            f.write("")
+
+        results = fs.is_file_exist("test.txt")
+        self.assertTrue(results)
+
+    def test_is_file_exist_in_dir(self):
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS, SALT, ITERATIONS)
+        fs.create()
+
+        fs.mkdir("/foobar")
+        with fs.open("foobar/test.txt", "w") as f:
+            f.write("")
+        results = fs.is_file_exist("foobar/test.txt")
+        self.assertTrue(results)
+
+    def test_is_file_not_exist(self):
+        fs = FileSystem(SECRET, WORKING_DIR, ITERATIONS, SALT, ITERATIONS)
+        fs.create()
+
+        results = fs.is_file_exist("test.txt")
+        self.assertFalse(results)
