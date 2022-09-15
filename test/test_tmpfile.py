a70b8a74e30be6d3ad250af37f37e7c264145688
lmoulin <lmoulin@spartan-conseil.fr>
2022-09-15 11:05:45 +0200

    check write back in case of exception

--- test/test_tmpfile.py
+++ test/test_tmpfile.py
@@ -10,7 +10,7 @@ WORKING_FILE = "/tmp/test.x"
 SECRET = b"secret"
 ITERATIONS = 100
 
-#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 class TestTmpFile(unittest.TestCase):
 
@@ -62,6 +62,28 @@ class TestTmpFile(unittest.TestCase):
         with tmp as filename:
             with open(filename, "w") as f:
                 f.write("test_read_utf8")
+
+        with BasicFile(WORKING_FILE, SECRET, "r", ITERATIONS) as f:
+            results = f.read()
+
+        expected = "test_read_utf8"
+
+        self.assertEqual(results, expected)
+
+    def test_write_with_exception(self):
+        with BasicFile(WORKING_FILE, SECRET, "w", ITERATIONS) as f:
+            f.write("")
+
+        tmp = TmpFile(SECRET,WORKING_FILE, "", ITERATIONS)
+
+        try:
+            with tmp as filename:
+                with open(filename, "w") as f:
+                    f.write("test_read_utf8")
+
+                raise Exception("Bad stuff here")
+        except:
+            print("something happen here")
 
         with BasicFile(WORKING_FILE, SECRET, "r", ITERATIONS) as f:
             results = f.read()
